5036dfecc467f673ed2608cf907e58260b9c46b1
Fourty4 <mutukuk04@gmail.com>
2023-06-22 14:19:41 -0700

    0 Task

--- 0x03-debugging/1-isdigit.c
+++ 0x03-debugging/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
+ * _isdigit - check if Character is a digit
+ * @x: The number to be checked
  *
  * Return: 0 or 1
  */
 
-int _isdigit(int c)
+int _isdigit(int x)
 {
-	if (c >= '0' && c <= '9')
+	if (x >= 48 && x <= 57)
 		return (1);
 	else
 		return (0);
